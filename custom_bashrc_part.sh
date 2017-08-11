802af069954c999bc46ca9f1be87841b823688e2
dockerdev <dockerdev@dockerdev.yellow.sophos>
2017-08-11 15:55:17 +0100

    first commit

---
+++ custom_bashrc_part.sh
@@ -0,0 +1,15 @@
+export PS1="\[$(tput bold)\]\[\033[38;5;47m\]\u@\h\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;134m\]\w\\n\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;51m\]==>  \[$(tput sgr0)\]"
+
+
+function set-title() {
+  if [[ -z "$ORIG" ]]; then
+    ORIG=$PS1
+  fi
+  TITLE="\[\e]2;$*\a\]"
+  PS1=${ORIG}${TITLE}
+}
+
+mcd () {
+    mkdir -p $1
+    cd $1
+}
